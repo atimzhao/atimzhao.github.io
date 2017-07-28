2867fbe2210bc8ebb17f8216840a73c1894771ab
Timothy Zhao <timothyzhao@Protim.local>
2017-07-27 17:04:20 -0700

    flairs added

--- js/grayscale.js
+++ js/grayscale.js
@@ -11,6 +11,7 @@ function collapseNavbar() {
     } else {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
+
 }
 
 $(window).scroll(collapseNavbar);
@@ -27,159 +28,207 @@ $(function() {
     });
 });
 
+$(window).scroll(function () { 
+    if ($(window).scrollTop() > 200) {
+      $('#disappear').fadeIn(2000);
+    }
+    if ($(window).scrollTop() < 200) {
+      $('#disappear').fadeOut(30);
+    }
+});
+    
+
 // Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
-    $(".navbar-collapse").collapse('hide');
+    $(".navbar-collapse").collapse('slow');
 });
+canvas = document.getElementById("canvas");
+context = canvas.getContext("2d");
+width = canvas.width = window.innerWidth;
+height = canvas.height = window.innerHeight;
 
-// Google Maps Scripts
-var map = null;
-// When the window has finished loading create our google map below
-google.maps.event.addDomListener(window, 'load', init);
-google.maps.event.addDomListener(window, 'resize', function() {
-    map.setCenter(new google.maps.LatLng(40.6700, -73.9400));
-});
 
-function init() {
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
-        zoom: 15,
+// Confetti Cannon
+particle = [];
+particleCount = 0,
+gravity = 0.3,
+    colors = [
+      '#f44336', '#e91e63', '#9c27b0', '#673ab7', '#3f51b5',
+      '#2196f3', '#03a9f4', '#00bcd4', '#009688', '#4CAF50',
+      '#8BC34A', '#CDDC39', '#FFEB3B', '#FFC107', '#FF9800',
+      '#FF5722', '#795548'
+    ];
 
-        // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
 
-        // Disables the default Google Maps UI components
-        disableDefaultUI: true,
-        scrollwheel: false,
-        draggable: false,
 
-        // How you would like to style the map.
-        // This is where you would paste any style found on Snazzy Maps.
-        styles: [{
-            "featureType": "water",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 17
-            }]
-        }, {
-            "featureType": "landscape",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 20
-            }]
-        }, {
-            "featureType": "road.highway",
-            "elementType": "geometry.fill",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 17
-            }]
-        }, {
-            "featureType": "road.highway",
-            "elementType": "geometry.stroke",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 29
-            }, {
-                "weight": 0.2
-            }]
-        }, {
-            "featureType": "road.arterial",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 18
-            }]
-        }, {
-            "featureType": "road.local",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 16
-            }]
-        }, {
-            "featureType": "poi",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 21
-            }]
-        }, {
-            "elementType": "labels.text.stroke",
-            "stylers": [{
-                "visibility": "on"
-            }, {
-                "color": "#000000"
-            }, {
-                "lightness": 16
-            }]
-        }, {
-            "elementType": "labels.text.fill",
-            "stylers": [{
-                "saturation": 36
-            }, {
-                "color": "#000000"
-            }, {
-                "lightness": 40
-            }]
-        }, {
-            "elementType": "labels.icon",
-            "stylers": [{
-                "visibility": "off"
-            }]
-        }, {
-            "featureType": "transit",
-            "elementType": "geometry",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 19
-            }]
-        }, {
-            "featureType": "administrative",
-            "elementType": "geometry.fill",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 20
-            }]
-        }, {
-            "featureType": "administrative",
-            "elementType": "geometry.stroke",
-            "stylers": [{
-                "color": "#000000"
-            }, {
-                "lightness": 17
-            }, {
-                "weight": 1.2
-            }]
-        }]
+            for( var i = 0; i < 300; i++){
+
+particle.push({
+    x : width/2,
+    y : height/2,
+    boxW : randomRange(5,20),
+    boxH : randomRange(5,20),
+    size : randomRange(2,8),
+    
+    spikeran:randomRange(3,5),
+    
+    velX :randomRange(-8,8),
+    velY :randomRange(-50,-10),
+    
+    angle :convertToRadians(randomRange(0,360)),
+    color:colors[Math.floor(Math.random() * colors.length)],
+    anglespin:randomRange(-0.2,0.2),
+    
+    draw : function(){
+    
+    
+        context.save();
+            context.translate(this.x,this.y);
+            context.rotate(this.angle);
+        context.fillStyle=this.color;
+        context.beginPath();
+        // drawStar(0, 0, 5, this.boxW, this.boxH);
+    context.fillRect(this.boxW/2*-1,this.boxH/2*-1,this.boxW,this.boxH);
+        context.fill();
+                context.closePath();
+        context.restore();
+    this.angle += this.anglespin;
+    this.velY*= 0.999;
+    this.velY += 0.3;
+  
+    this.x += this.velX;
+    this.y += this.velY;
+        if(this.y < 0){
+    this.velY *= -0.2;
+        this.velX *= 0.9;
     };
+    if(this.y > height){
+    this.anglespin = 0;
+    this.y = height;
+    this.velY *= -0.2;
+        this.velX *= 0.9;
+    };
+        if(this.x > width ||this.x< 0){
 
-    // Get the HTML DOM element that will contain your map
-    // We are using a div with id="map" seen below in the <body>
-    var mapElement = document.getElementById('map');
+    this.velX *= -0.5;
+    };
+    
+    
+    
+    },
 
-    // Create the Google Map using out element and options defined above
-    map = new google.maps.Map(mapElement, mapOptions);
 
-    // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
-    var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
-    var beachMarker = new google.maps.Marker({
-        position: myLatLng,
-        map: map,
-        icon: image
+
+        
     });
+
 }
+r1={
+x:width/2-150,
+y:height/2-150,
+width:300,
+height:300,
+    velX :0,
+    velY :-10,
+ img : loadImage(""),
+ alphatop:0
+};
+
+
+function drawScreen(){
+size = 50;
+            pFontName = "Lucida Sans Unicode";
+            context.font = size + "pt " + pFontName;
+            context.fillText("", width/2,150 );
+        if(r1.alphatop < 1){
+        r1.alphatop += 0.01;
+        }else{
+        r1.alphatop = 1;
+        }
+        context.globalAlpha = r1.alphatop; 
+            context.drawImage(r1.img,r1.x,r1.y);
+                  context.textAlign = 'center';
+
+            
+            
+            if(r1.alphatop === 1){
+    r1.velY*= 0.999;
+    r1.velY += 0.3;
+  
+    r1.x += r1.velX;
+    r1.y += r1.velY;}
+
+        if(r1.y + r1.height > height){
+    r1.anglespin = 0;
+    r1.y = height - r1.height;
+    r1.velY *= -0.8;
+        r1.velX *= 0.9;
+    };
+    
+    
+            context.globalAlpha = 1; 
+        for( var i = 0; i < particle.length; i++){
+            particle[i].draw();
+
+                }
+                
+                
+}
+
+    function loadImage(url){
+        var img = document.createElement("img");
+        img.src=url;
+        return img;
+    }
+
+function update(){
+
+
+context.clearRect(0,0,width,height);
+
+drawScreen();
+
+requestAnimationFrame(update);
+}
+
+update();
+
+
+function randomRange(min, max){
+    return min + Math.random() * (max - min );
+}
+
+function randomInt(min, max){
+    return Math.floor(min + Math.random()* (max - min + 1));
+}
+
+       function convertToRadians(degree) {
+            return degree*(Math.PI/180);
+        }
+        
+        function drawStar(cx, cy, spikes, outerRadius, innerRadius,color) {
+    var rot = Math.PI / 2 * 3;
+    var x = cx;
+    var y = cy;
+    var step = Math.PI / spikes;
+
+    context.strokeSyle = "#000";
+    context.beginPath();
+    context.moveTo(cx, cy - outerRadius)
+    for (i = 0; i < spikes; i++) {
+        x = cx + Math.cos(rot) * outerRadius;
+        y = cy + Math.sin(rot) * outerRadius;
+        context.lineTo(x, y)
+        rot += step
+
+        x = cx + Math.cos(rot) * innerRadius;
+        y = cy + Math.sin(rot) * innerRadius;
+        context.lineTo(x, y)
+        rot += step
+    }
+    context.lineTo(cx, cy - outerRadius)
+    context.closePath();
+    context.fillStyle=color;
+    context.fill();
+
+}       
